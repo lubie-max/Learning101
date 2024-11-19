e7c4d00ad0bd9601f4e3e0912ce7196c3d34f9d7
lubie-max <shaikhls2421@gmail.com>
2024-11-19 23:19:39 +0530

    first commit

---
+++ learning101/src/test/java/com/learning101/learning101/Learning101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning101.learning101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learning101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
